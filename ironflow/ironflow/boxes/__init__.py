0f2dba3eaef5aa982c76023dd68e6c6ede890944
Liam Huber <liam.huber@gmail.com>
2022-09-29 20:46:54 -0700

    Improve hiding of text output

--- ironflow/ironflow/boxes/__init__.py
+++ ironflow/ironflow/boxes/__init__.py
@@ -1,3 +1,3 @@
 from ironflow.ironflow.boxes.node_interface import NodeController, NodePresenter
-from ironflow.ironflow.boxes.text import TextOut, TextIn
+from ironflow.ironflow.boxes.text_output import TextOut, TextIn
 from ironflow.ironflow.boxes.toolbar import Toolbar
--- ironflow/ironflow/boxes/text_output.py
+++ ironflow/ironflow/boxes/text_output.py
@@ -13,42 +13,35 @@ from typing import Callable, Optional, Any
 
 class TextOut:
     def __init__(self):
+        layout = widgets.Layout(
+            width="100%",
+            border="1px solid black",
+        )
+        self._box = widgets.VBox([], layout=layout)
         self._output = widgets.Output()
         self._button = widgets.Button(
             tooltip="Clear output",
             description="clear",
-            layout={"width": "100px", "visibility": "hidden"},
+            layout={"width": "100px"},
         )
         self._button.on_click(self._click_button)
 
     def clear(self):
         self._output.clear_output()
-        self._hide_button()
+        self._box.children = []
 
     def _click_button(self, change: dict) -> None:
         self.clear()
 
-    def _hide_button(self):
-        self._button.layout.visibility = "hidden"
-        self._button.layout.height = "0px"
-
-    def _show_button(self):
-        self._button.layout.visibility = "visible"
-        self._button.layout.height = "auto"
-
     @property
     def box(self) -> widgets.VBox:
-        layout = widgets.Layout(
-            width="100%",
-            border="1px solid black",
-        )
-        return widgets.VBox([self._output, self._button], layout=layout)
+        return self._box
 
     def print(self, msg: str):
         self._output.clear_output()
         with self._output:
             display(msg)
-        self._show_button()
+        self._box.children = [self._output, self._button]
 
 
 class TextIn(Box):
