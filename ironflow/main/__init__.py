1b624df2e6a29d7e3d9c6d8315e90b1827e57bd9
Liam Huber <liam.huber@gmail.com>
2022-10-14 14:20:01 -0700

    Add module docstring

--- ironflow/main/__init__.py
+++ ironflow/main/__init__.py
@@ -1,3 +1,10 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+"""
+Wrappers and extensions of the underlying ryven model.
+"""
+
 from ryvencore import (
     NodeInputBP,
     NodeOutputBP,
