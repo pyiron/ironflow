ba452700d73894721cb56d12e086ef301f275f32
Liam Huber <liam.huber@gmail.com>
2022-10-28 17:11:02 -0700

    Remove the constraint entirely
    Although making pip happy might be a vain effort, here is the error message:
    The conflict is caused by:
        pyiron-atomistics 0.2.59 depends on mendeleev>=0.11.0
        pyiron-atomistics 0.2.58 depends on mendeleev>=0.11.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'nglview',
         'numpy',
         'pyiron_base',
-        'pyiron_atomistics >= 0.2.58',
+        'pyiron_atomistics',
         'ryvencore'
     ],
     cmdclass=versioneer.get_cmdclass(),
