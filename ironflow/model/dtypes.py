1e94b33eb9005d39ffcfad41e8634d270d1a91dc
liamhuber <liamhuber@greyhavensolutions.com>
2022-11-22 13:39:24 -0800

    Make a new base DType with type checking

--- ironflow/model/dtypes.py
+++ ironflow/model/dtypes.py
@@ -7,9 +7,56 @@ Wrapped dtypes to avoid mutable defaults.
 Implementation changes in ryvencore v0.4, so this file may be short-lived.
 """
 
-from typing import Optional
+from __future__ import annotations
 
-from ryvencore.dtypes import DType, Data, Integer, Float, String, Boolean, Char
+from typing import Any, Optional
+
+from ryvencore.dtypes import (
+    DType as DTypeCore, Data, Integer, Float, String, Boolean, Char
+)
+
+
+class DType(DTypeCore):
+    def __init__(
+            self,
+            default,
+            bounds: tuple = None,
+            doc: str = "",
+            _load_state=None,
+            valid_classes=None,
+            allow_none=False
+    ):
+        super().__init__(
+            default=default,
+            bounds=bounds,
+            doc=doc,
+            _load_state=_load_state,
+        )
+        if isinstance(valid_classes, list):
+            self.valid_classes = valid_classes
+        elif valid_classes is not None:
+            self.valid_classes = [valid_classes]
+        else:
+            self.valid_classes = None
+        self.allow_none = allow_none
+
+    def _dtype_matches(self, val: DType):
+        # val can be *more* specific, but not *less*
+        return isinstance(val, self.__class__)
+
+    def _instance_matches(self, val: Any):
+        if self.valid_classes is not None:
+            return any([isinstance(val, c) for c in self.valid_classes])
+        else:
+            return False
+
+    def matches(self, val: DType | Any | None):
+        if isinstance(val, DType):
+            return self._dtype_matches(val)
+        elif val is not None:
+            return self._instance_matches(val)
+        else:
+            return self.allow_none
 
 
 class Choice(DType):
