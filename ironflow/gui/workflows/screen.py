734e0bed0c91dffa69ed796b16a452ff0aa24350
liamhuber <liamhuber@greyhavensolutions.com>
2023-01-12 11:11:39 -0800

    Refactor node presenter to look more like node controller

--- ironflow/gui/workflows/screen.py
+++ ironflow/gui/workflows/screen.py
@@ -119,17 +119,17 @@ class WorkflowsGUI(Screen):
             self.node_controller.draw_for_node(None)
 
     def open_node_presenter(self, node_widget: NodeWidget):
-        self.node_presenter.node_widget = node_widget
+        self.node_presenter.draw_for_node_widget(node_widget)
 
     def update_node_presenter(self):
-        self.node_presenter.draw()
+        self.node_presenter.update()
 
     def close_node_presenter(self):
-        self.node_presenter.close()
+        self.node_presenter.draw_for_node_widget(None)
 
     def ensure_node_not_presented(self, node_widget: NodeWidget) -> None:
         if self.node_presenter.node_widget == node_widget:
-            self.node_presenter.node_widget = None
+            self.node_presenter.draw_for_node_widget(None)
 
     def redraw_active_flow_canvas(self):
         self.flow_canvas.redraw()
@@ -183,7 +183,7 @@ class WorkflowsGUI(Screen):
         file_name = self.input.text
         self.model.load(f"{file_name}.json")
         self.update_tabs()
-        self.node_presenter.clear_output()
+        self.node_presenter.clear()
         self.print(f"Session loaded from {file_name}.json")
         self.input.clear()
 
