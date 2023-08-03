3c32b78382986b2e9da9938ad2d24ebd360e12f9
liamhuber <liamhuber@greyhavensolutions.com>
2023-08-03 09:26:39 -0700

    Add 311 support

--- setup.py
+++ setup.py
@@ -22,7 +22,9 @@ setup(
                  'Intended Audience :: Science/Research',
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 3.9',
-                 'Programming Language :: Python :: 3.10'],
+                 'Programming Language :: Python :: 3.10',
+                 'Programming Language :: Python :: 3.11',
+                 ],
 
     keywords='pyiron',
     packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
